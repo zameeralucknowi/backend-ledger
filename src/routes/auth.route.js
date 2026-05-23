1c3621727bca8c91dfcb69a7fc49beed6145ff1e
zameer <zameeralucknowi@gmail.com>
2026-05-23 19:56:43 +0530

    added login

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router();
 const {signupController }  = require('../controllers/auth.controller');
 
-router.post('/signup',signupController)
+router.post('/signup',signupController);
+router.post('/login',)
 
 
 module.exports = router;
