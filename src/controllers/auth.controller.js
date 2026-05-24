aa91deed3dda8b5300d9da0cc6e2d66d3000c70a
zameer <zameeralucknowi@gmail.com>
2026-05-24 13:15:26 +0530

    login controller added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,20 @@
 const User = require('../models/user.model')
+const bcrypt = require('bcryptjs')
 
 const signupController = async (req,res) =>{
     try {    
         const {email,password,name} = req.body;
+        const isExists = await User.findOne({email:email});
+        if(isExists){
+            return res.status(409).json({
+                message : 'user already exists',
+                status : 'failed'
+            })
+        }
+        const hashPassword = await bcrypt.hash(password,10);
         const newUser = new User({
             email : email,
-            password : password,
+            password : hashPassword,
             name : name
         })
         await newUser.save();
@@ -18,6 +27,33 @@ const signupController = async (req,res) =>{
     }
 }
 
+const loginController = async(req,res)=>{
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email:email});
+        if(!user){
+            return res.status(404).json({
+                message : 'user not found',
+                status:'failed'          
+            })
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(401).json({
+                message : 'invalid credentials',
+                status : 'failed'
+            })
+        }
+        return res.status(200).json({
+            message : 'user loggedin successfully',
+            data:user
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
-    signupController
+    signupController,
+    loginController
 }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router();
-const {signupController }  = require('../controllers/auth.controller');
+const {signupController,loginController }  = require('../controllers/auth.controller');
 
 router.post('/signup',signupController);
-router.post('/login',)
+router.post('/login',loginController);
 
 
 module.exports = router;
